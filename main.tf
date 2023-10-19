f18724315a4c4a515fb55ee251e455a9672aa534
Dmytro <tarakanovds@gmail.com>
2023-10-20 01:58:52 +0300

    - fixed

---
+++ main.tf
@@ -0,0 +1,5 @@
+resource "github_repository_collaborator" "a_repo_collaborator" {
+  repository = "github-terraform-task-Bronsborought"
+  username   = "softservedata"
+  permission = "collaborator"
+}
