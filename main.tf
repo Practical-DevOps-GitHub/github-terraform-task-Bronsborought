3f6d99f9cdb56240975c43c4b229f711da6b9ea1
Dmytro <tarakanovds@gmail.com>
2023-10-20 02:07:56 +0300

    - fixed

--- main.tf
+++ main.tf
@@ -3,3 +3,19 @@ resource "github_repository_collaborator" "a_repo_collaborator" {
   username   = "softservedata"
   permission = "collaborator"
 }
+
+resource "github_repository" "example" {
+  name        = "github-terraform-task-Bronsborought"
+  description = "My awesome codebase"
+  auto_init   = true
+}
+
+resource "github_branch" "develop" {
+  repository = github_repository.example.name
+  branch     = "develop"
+}
+
+resource "github_branch_default" "default"{
+  repository = github_repository.example.name
+  branch     = github_branch.develop.branch
+}
